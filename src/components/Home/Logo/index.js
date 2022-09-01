8c7fac2a4073f2bc53380569dbfe6a6c8304dad4
Yevhen Reshetar <reshetar@YevhenMacBook-3063.local>
2022-09-01 16:43:46 +0300

    update gsap3

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,39 +1,39 @@
 import { useEffect, useRef } from 'react'
 import gsap from 'gsap'
-import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
+// import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
 import HeroPic from '../../../assets/images/hero-pic.png'
 import './index.scss'
 
 const Logo = () => {
   const bgRef = useRef()
-  const outlineLogoRef = useRef()
+  // const outlineLogoRef = useRef()
   const solidLogoRef = useRef()
 
   useEffect(() => {
-    gsap.registerPlugin(DrawSVGPlugin)
+    // gsap.registerPlugin(DrawSVGPlugin)
 
-    gsap
-      .timeline()
-      .to(bgRef.current, {
-        duration: 1,
-        opacity: 1,
-      })
-      .from(outlineLogoRef.current, {
-        drawSVG: 0,
-        duration: 20,
-      })
+    // gsap
+    //   .timeline()
+    //   .to(bgRef.current, {
+    //     duration: 1,
+    //     opacity: 1,
+    //   })
+    //   .from(outlineLogoRef.current, {
+    //     drawSVG: 0,
+    //     duration: 20,
+    //   })
 
-    gsap.fromTo(
-      solidLogoRef.current,
-      {
-        opacity: 0,
-      },
-      {
-        opacity: 1,
-        delay: 1,
-        duration: 4,
-      }
-    )
+    // gsap.fromTo(
+    //   solidLogoRef.current,
+    //   {
+    //     opacity: 0,
+    //   },
+    //   {
+    //     opacity: 1,
+    //     delay: 1,
+    //     duration: 4,
+    //   }
+    // )
   }, [])
 
   return (
