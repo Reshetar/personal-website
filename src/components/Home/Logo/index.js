17be8fb5d4342488f6995456ef2f62b39ad7e1e5
Yevhen Reshetar <reshetar@YevhenMacBook-3063.local>
2022-09-01 16:30:49 +0300

    update gsap

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,5 +1,5 @@
 import { useEffect, useRef } from 'react'
-import gsap from 'gsap-trial'
+import gsap from 'gsap-trial/dist/gsap'
 import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
 import HeroPic from '../../../assets/images/hero-pic.png'
 import './index.scss'
